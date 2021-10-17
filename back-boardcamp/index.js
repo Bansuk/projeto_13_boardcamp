ac159a186b6f85375bafac6fb6360800b9e89305
Bruno Balbuena <bbalbuena@pop-os.localdomain>
2021-10-17 00:41:47 -0300

    Inserir categorias

--- back-boardcamp/index.js
+++ back-boardcamp/index.js
@@ -11,18 +11,32 @@ app.get("/categories", async (req, res) => {
     try {
         const result = await connection.query("SELECT * FROM categories;");
         res.send(result.rows);
-    } catch (error) {}
+    } catch (error) {
+        res.sendStatus(500);
+    }
 });
 
 app.post("/categories", async (req, res) => {
     const { name } = req.body;
 
-    try {
-        await connection.query("INSERT INTO categories (name) VALUES ($1);", [
-            name,
-        ]);
-        res.status(201).send("sem dados");
-    } catch (error) {}
+    const result = await connection.query(
+        "SELECT * FROM categories WHERE name = $1;",
+        [name]
+    );
+
+    if (!name) res.sendStatus(400);
+    else if (result.rowCount) res.sendStatus(409);
+    else {
+        try {
+            await connection.query(
+                "INSERT INTO categories (name) VALUES ($1);",
+                [name]
+            );
+            res.sendStatus(201);
+        } catch (error) {
+            res.sendStatus(500);
+        }
+    }
 });
 
 app.listen(4000);
